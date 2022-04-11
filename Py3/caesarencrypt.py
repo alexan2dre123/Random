9ec2d452f19326a3aa480f814dde28a2a1b24844
Alexandre Beck <34386707+alexan2dre123@users.noreply.github.com>
2022-04-11 20:16:18 -0300

    Add files via upload

---
+++ Py3/caesarencrypt.py
@@ -0,0 +1,57 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = [' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(text,shift):
+  encrypted = ""
+  for letter in text:
+      index = alphabet.index(letter)
+      new_index = index + shift
+      new_letter = alphabet[new_index]
+      encrypted += new_letter
+  return encrypted
+
+def decrypt(encrypted, shift):
+    decrypted = ""
+    for letter in encrypted:
+        index = alphabet.index(letter)
+        new_index = index - shift
+        new_letter = alphabet[new_index]
+        decrypted += new_letter
+    return decrypted
+
+print(logo)
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 27
+    if direction == "encode":
+        encrypted = encrypt(text, shift)
+        print(f"Your encoded message is {encrypted}")
+    if direction == "decode":
+        decrypted = decrypt(encrypted,shift)
+        print(f"Your decoded message is {decrypted}")
+    user_continue = input("Do you want to continue? (Y/N)").lower()
+    if user_continue == "y":
+        continue
+    else:
+        break
+
+
